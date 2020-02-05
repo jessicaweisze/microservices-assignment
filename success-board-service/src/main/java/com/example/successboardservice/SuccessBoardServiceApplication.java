19ed3a071c33db97879cf50f6e7b693aab0a01ec
Jessi <jessica.weisze@googlemail.com>
2020-02-05 19:44:23 +0100

    kein localhost

--- success-board-service/src/main/java/com/example/successboardservice/SuccessBoardServiceApplication.java
+++ success-board-service/src/main/java/com/example/successboardservice/SuccessBoardServiceApplication.java
@@ -3,7 +3,10 @@ package com.example.successboardservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @EnableCircuitBreaker
 @EnableEurekaClient
@@ -12,6 +15,12 @@ public class SuccessBoardServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SuccessBoardServiceApplication.class, args);
+	}
+
+	@LoadBalanced
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
 	}
 
 }
--- success-board-service/src/main/java/com/example/successboardservice/service/ResolutionCommunicatorService.java
+++ success-board-service/src/main/java/com/example/successboardservice/service/ResolutionCommunicatorService.java
@@ -3,6 +3,7 @@ package com.example.successboardservice.service;
 import com.example.successboardservice.web.model.ResolutionItem;
 import com.example.successboardservice.web.model.ResolutionUser;
 import com.example.successboardservice.web.model.UserResolutionItem;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -13,6 +14,8 @@ import java.util.stream.Collectors;
 
 @Service
 public class ResolutionCommunicatorService {
+
+    @Autowired
     private final RestTemplate template;
     //private final String url;
 
@@ -21,62 +24,62 @@ public class ResolutionCommunicatorService {
         //this.url = url;
     }
     public List<ResolutionItem> findAllResolutions() {
-        final String requestUrl = "http://localhost:8082" + "/todoresolutions";
+        final String requestUrl = "http://todo-board-service" + "/todoresolutions";
         final List<ResolutionItem> response = this.template.getForObject(requestUrl, List.class);
 
         return response;
     }
 
     public List<ResolutionItem> findResolutionsByUser(Integer userId) {
-        final String requestUrl = "http://localhost:8082" + "/todoresolutions/" + userId;
+        final String requestUrl = "http://todo-board-service" + "/todoresolutions/" + userId;
         final List<ResolutionItem> response = this.template.getForObject(requestUrl, List.class);
 
         return response;
     }
 
     public List<UserResolutionItem> getUserResolutions() {
-        final String requestUrl = "http://localhost:8082" + "/todoresolutions";
+        final String requestUrl = "http://todo-board-service" + "/todoresolutions";
         List<ResolutionItem> iteml = this.template.getForObject(requestUrl, List.class);
         return iteml.stream()
                 .map(items -> {
-                    ResolutionUser resolutionUser = this.template.getForObject("http://localhost:8083/users/" + items.getUserId(), ResolutionUser.class);
+                    ResolutionUser resolutionUser = this.template.getForObject("http://resolutionUser-service/users/" + items.getUserId(), ResolutionUser.class);
                     return new UserResolutionItem(items.getTitle(), items.getDescription(), items.getStatus(), resolutionUser.getResUserName());
                 })
                 .collect(Collectors.toList());
     }
 
     public ResolutionItem saveResolutionItem(ResolutionItem resolutionItem) {
-        final String requestUrl = "http://localhost:8082" + "/todoresolutions/create";
+        final String requestUrl = "http://todo-board-service" + "/todoresolutions/create";
         final ResolutionItem response = this.template.postForObject(requestUrl, resolutionItem, ResolutionItem.class);
         return response;
     }
 
     public List<ResolutionUser> findAllUser() {
-        final String requestUrl = "http://localhost:8083" + "/users";
+        final String requestUrl = "http://resolutionUser-service" + "/users";
         final List<ResolutionUser> response = this.template.getForObject(requestUrl, List.class);
         return response;
 
     }
     public ResolutionUser saveUser(ResolutionUser resolutionUser) {
-        final String requestUrl = "http://localhost:8083" + "/users/add";
+        final String requestUrl = "http://resolutionUser-service" + "/users/add";
         final ResolutionUser response = this.template.postForObject(requestUrl, resolutionUser, ResolutionUser.class);
         return response;
     }
 
     public ResolutionUser deleteUser(Integer userId) {
-        final String requestUrl = "http://localhost:8083" + "/users/delete/" + userId;
+        final String requestUrl = "http://resolutionUser-service" + "/users/delete/" + userId;
         final ResponseEntity<ResolutionUser> response = this.template.exchange(requestUrl, HttpMethod.DELETE, null, ResolutionUser.class);
         return response.getBody();
     }
 
     public ResolutionItem get(Integer itemId) {
-        final String requestUrl = "http://localhost:8082" + "/resolutions/"+ itemId;
+        final String requestUrl = "http://todo-board-service" + "/resolutions/"+ itemId;
         final ResolutionItem response = this.template.getForObject(requestUrl, ResolutionItem.class);
         return response;
     }
 
     public ResolutionItem deleteResolutionItem(Integer itemId) {
-        final String requestUrl = "http://localhost:8082" + "/delete/"+ itemId;
+        final String requestUrl = "http://todo-board-service" + "/delete/"+ itemId;
         final ResponseEntity<ResolutionItem> response = this.template.exchange(requestUrl, HttpMethod.DELETE, null, ResolutionItem.class);
         return response.getBody();
     }
