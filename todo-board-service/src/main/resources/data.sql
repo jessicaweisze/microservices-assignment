c1855a2acbe8f82eda4d904011a420321c5cabfc
Jessi <jessica.weisze@googlemail.com>
2020-02-12 19:41:15 +0100

    mysql data

---
+++ todo-board-service/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT into resolution_item (item_id, description, status, title, user_id) VALUES (1, 'Mind. 100 Schafe sehen,weil Schafe flauschig sind.', 'todo', 'Viele Schafe sehen', 1);
+INSERT into resolution_item (item_id, description, status, title, user_id) VALUES (2, 'Mind. 3 pro Woche mit dem Lichtschwert üben.', 'todo', 'Besser werden mit dem Lichtschwert', 1);
+INSERT into resolution_item (item_id, description, status, title, user_id) VALUES (3, 'Alle retten.', 'done', 'Todesstern zerstören.', 2);
+INSERT into resolution_item (item_id, description, status, title, user_id) VALUES (4, '20 Sternenkekse backen.', 'todo', 'Backen', 2);
+INSERT into resolution_item (item_id, description, status, title, user_id) VALUES (5, 'Mind. 10 Bücher über Droiden lesen.', 'todo', 'Droiden verstehen', 1);
+INSERT into resolution_item (item_id, description, status, title, user_id) VALUES (6, 'beep beep beeeeeeeep.', 'done', 'beep', 3);
+INSERT into resolution_item (item_id, description, status, title, user_id) VALUES (7, 'Einfach weg.', 'todo', 'Verschwinden', 1);
---
+++ user-service/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT into res_user_table (res_user_id, res_user_name) VALUES (1, 'Luke');
+INSERT into res_user_table (res_user_id, res_user_name) VALUES (2, 'Yoda');
+INSERT into res_user_table (res_user_id, res_user_name) VALUES (3, 'R2D2');
